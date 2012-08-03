6f1cd8a00d0d31c1cacbb49b4c1848c388c51563
Trevor Kimenye <kimenye@gmail.com>
2012-08-03 14:03:39 +0300

    Added start and restart button

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -5,30 +5,39 @@ $(document).ready(function () {
         var self = this;
 
         self.deck = ko.observable(null);
-        self.computer = new ComputerPlayer();
-        self.user = ko.observable(new UserPlayer("Player 1"));
-        self.inProgress = ko.observable(false);
-        self.board = ko.observable(new Board());
-        if (self.deck() == null) {
-            self.deck(new Deck());
-            self.deck().display();
+        self.initGame = function() {
+            self.computer = new ComputerPlayer();
+            self.user = ko.observable(new UserPlayer("Player 1"));
+            self.inProgress = ko.observable(false);
+            self.board = ko.observable(new Board());
+            if (self.deck() == null) {
+                self.deck(new Deck());
+                self.deck().display();
+            }
+
+            self.game = ko.observable(new Game(self.user(), self.computer, self.board(), self.deck()));
+
+            self.playerCanMove = ko.computed(function() {
+                var hasSelections = self.user().hasSelections();
+                return hasSelections && self.playerCanPlay();
+            });
+
+            self.playerCanPlay = ko.computed(function() {
+                return self.user().isTurnToPlay();
+            });
         }
 
-        self.game = ko.observable(new Game(self.user(), self.computer, self.board(), self.deck()));
-
-        self.playerCanMove = ko.computed(function() {
-            var hasSelections = self.user().hasSelections();
-            return hasSelections && self.playerCanPlay();
-        });
-
-        self.playerCanPlay = ko.computed(function() {
-           return self.user().isTurnToPlay();
-        });
+        self.initGame();
 
         self.startGame = function() {
             self.shuffle();
             self.deal();
             self.inProgress(true);
+        }
+
+        self.restartGame = function() {
+            self.initGame();
+            self.startGame();
         }
 
         self.shuffle = function() {
@@ -74,5 +83,5 @@ $(document).ready(function () {
     }
     var vm = new GameVM()
     ko.applyBindings(vm);
-    vm.startGame();
+//    vm.startGame();
 });
